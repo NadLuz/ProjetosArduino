5ca6e9cbc9716778bbd5d3b7743734289e218bbe
nadja <nadja.pluz@gmail.com>
2024-06-24 12:26:02 -0300

    Update Programa2-LEDeBotao.ino
    Adicionado os comentários nas linhas do programa e correção de erros

--- Programa2-LEDeBotao/Programa2-LEDeBotao.ino
+++ Programa2-LEDeBotao/Programa2-LEDeBotao.ino
@@ -1,22 +1,31 @@
+// definição das variáveis do programa
 int botao;
 int estadoAnterior = 0;
 int estadoBotao = LOW;
 
 void setup() {
+  // definição da porta de saída
   pinMode (2,OUTPUT);
+  // definição da porta de entrada
   pinMode (5,INPUT);
   
 }
 
 void loop() {
+  // lê a informação na porta 5 e armazena na variável 'botao'
   botao = digitalRead(5);
-  if (botao == HIGH && anterior == LOW){
+
+  //se o botao estiver com nível HIGH e o valor anterior for LOW   
+  if (botao == HIGH && estadoAnterior == LOW){
+    // interte o valor do estadoBotao
     estadoBotao =! estadoBotao;
   }
 
+  // escreve na porta 2 o valor que estiver em estadoBotao
   digitalWrite (2, estadoBotao);
 
-  anterior = valor;
+  // escreve na variável estadoAnterior qual o valor em 'botao'
+  estadoAnterior = botao;
   delay(50);
 
 }
