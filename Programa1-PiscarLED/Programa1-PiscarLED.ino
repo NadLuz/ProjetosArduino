44ba18dfa45341c057b49589799bddf9eefb8536
nadja <nadja.pluz@gmail.com>
2024-06-24 12:05:25 -0300

    Adicionado as linhas de comentários no programa

--- Programa1-PiscarLED/Programa1-PiscarLED.ino
+++ Programa1-PiscarLED/Programa1-PiscarLED.ino
@@ -1,12 +1,19 @@
+//Programa que faz piscar um LED a cada 1 segundo
+
 void setup() {
+  //Definição da porta onde será conectado o LED
   pinMode(7,OUTPUT);
 
 }
 
 void loop() {
+  //comando que envia sinal alto para a porta
   digitalWrite(7,HIGH);
+  //tempo de espera
   delay(1000);
+  //comando que envia sinal baixo para a porta
   digitalWrite(7, LOW);
+  //tempo de espera
   delay(1000);
 
 }
